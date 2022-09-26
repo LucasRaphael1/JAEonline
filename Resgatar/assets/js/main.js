a8b9f3436c9a1359f96c1fc3e6ef78198ea1b94d
LucasRaphael1 <lucasraphaelv77@gmail.com>
2022-09-26 19:23:04 -0300

    Atualização

--- Resgatar/assets/js/main.js
+++ Resgatar/assets/js/main.js
@@ -1,31 +1,3 @@
-const body = document.querySelector('body'),
-  sidebar = body.querySelector('nav'),
-  toggle = body.querySelector(".toggle"),
-  searchBtn = body.querySelector(".search-box"),
-  modeSwitch = body.querySelector(".toggle-switch"),
-  modeText = body.querySelector(".mode-text");
-
-
-toggle.addEventListener("click" , () =>{
-sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click" , () =>{
-sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-body.classList.toggle("dark");
-
-if(body.classList.contains("dark")){
-    modeText.innerText = "Light mode";
-}else{
-    modeText.innerText = "Dark mode";
-    
-}
-});
-
-//Timer (Contagem Regressiva)
 future = Date.parse("jun 12, 2022 01:30:00");
 now = new Date();
 diff = future - now;
@@ -45,7 +17,7 @@ document.getElementById("timer")
 '<div>' + s + '<span>Seconds</span></div>';
 setInterval('updateTimer()', 1000); 
 function updateTimer() {
-  future = Date.parse("jun 12, 2022 01:30:00");
+  future = Date.parse("sep 27, 2022 09:00:00");
 now = new Date();
 diff = future - now;
 
@@ -67,3 +39,35 @@ document.getElementById("timer")
 '<div>' + s + '<span>Seconds</span></div>';
 }
 setInterval('updateTimer()', 1000);
+
+//Notificação
+const a = document.querySelector(".button"),
+      toast = document.querySelector(".toast")
+      closeIcon = document.querySelector(".close"),
+      progress = document.querySelector(".progress");
+
+      let timer1, timer2;
+
+      a.addEventListener("click", () => {
+        toast.classList.add("active");
+        progress.classList.add("active");
+
+        timer1 = setTimeout(() => {
+            toast.classList.remove("active");
+        }, 5000); //1s = 1000 milliseconds
+
+        timer2 = setTimeout(() => {
+          progress.classList.remove("active");
+        }, 5300);
+      });
+      
+      closeIcon.addEventListener("click", () => {
+        toast.classList.remove("active");
+        
+        setTimeout(() => {
+          progress.classList.remove("active");
+        }, 300);
+
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+      });
