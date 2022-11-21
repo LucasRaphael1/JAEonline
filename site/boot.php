9e4eedf047f60705a5f1f6ddd3c1e00681bd00e0
LucasRaphael1 <112517549+LucasRaphael1@users.noreply.github.com>
2022-11-21 12:42:06 -0300

    Delete site directory

--- site/boot.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-define(CONF_DB_HOST, "localhost");
-define(CONF_DB_NAME, "alunos");
-define(CONF_DB_USER, "root");
-define(CONF_DB_PASS, "");
--- site/site/sair/sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['cgm']);
-    unset($_SESSION['senha']);
-    header('Location: ../login.php');
-?>
